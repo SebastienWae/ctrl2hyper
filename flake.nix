4bfbc6cf0b69a1f7399e35d325434fce069ae3a1
Sebastien Waegeneire <sebastien@waegeneire.com>
2021-07-23 11:14:27 +0200

    add nix support

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Plugin for Interception Tools that remaps Left Ctrl to Hyper.";
+
+  inputs.flake-utils.url = github:numtide/flake-utils;
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in rec {
+        packages.ctrl2hyper = pkgs.stdenv.mkDerivation rec {
+          pname = "ctrl2hyper";
+          version = "0.1";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "sebastienwae";
+            repo = "ctrl2hyper";
+            rev = "v${version}";
+            sha256 = "sha256-+nl2ChCRY/jDqFDgef4oqKocvsSNV4LpzIBEOYLiOls=";
+          };
+
+          nativeBuildInputs = [ pkgs.cmake ];
+        }; 
+        defaultPackage = packages.ctrl2hyper;
+      }
+    );
+}
