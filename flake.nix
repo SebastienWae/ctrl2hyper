5ba6709729d0ec7b3725f78a5457d5378b5e1898
Sebastien <55792872+SebastienWae@users.noreply.github.com>
2022-09-12 18:43:41 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
             owner = "sebastienwae";
             repo = "ctrl2hyper";
             rev = "v${version}";
-            sha256 = "sha256-QkXpBfqJQv3uedJHml99Kz6oX750oUoQpR0MJLbUGVE=";
+            sha256 = "sha256-QkXpBfqJQv3uedJHel99Kz6oX750oUoQpR0MJLbUGVE=";
           };
 
           nativeBuildInputs = [ pkgs.cmake ];
